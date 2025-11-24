240a4bb63b05d9003ae1aae1e0c8dfea920789ab
JlaraCast <163551527+JlaraCast@users.noreply.github.com>
2025-11-24 00:19:47 -0600

    Fix api/index.php bootstrap and use rewrites in vercel.json

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Register the Composer autoloader...
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
